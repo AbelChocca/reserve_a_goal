ba9962d9809baeb8972002c2c439780da1177746
AbelChocca <abelchocca1010@gmail.com>
2026-05-01 16:13:58 -0500

    fix: update import path for PrismaService in PrismaModule

--- prisma/prisma.module.ts
+++ prisma/prisma.module.ts
@@ -1,5 +1,5 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaService } from 'src/infra/prisma/prisma.service';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Global()
 @Module({
