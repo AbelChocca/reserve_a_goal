4cb970821e3767b126167d39fb06771cb4d35af8
AbelChocca <abelchocca1010@gmail.com>
2026-05-11 21:05:34 -0500

    feat: enhance AppModule and AppController with public access and additional modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 import { LocationModule } from './location/location.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { SpaceModule } from './space/space.module';
+import { AuthGuard } from './auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { AzureStorageModule } from './common/storage/azure-storage.module';
 
 @Module({
   imports: [
@@ -13,8 +17,16 @@ import { AuthModule } from './auth/auth.module';
     }),
     LocationModule,
     AuthModule,
+    SpaceModule,
+    AzureStorageModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
